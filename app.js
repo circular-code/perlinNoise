8029646538a42131f930f7301534a322f28df228
RocketScience <stephanos@malikotsinas.de>
2020-05-01 16:36:49 +0200

    1d perlin noise 2d representation

--- app.js
+++ app.js
@@ -4,46 +4,29 @@ const canvas = document.getElementById("canvas");
 const width = canvas.width = document.body.clientWidth;
 const height = canvas.height = document.body.clientHeight;
 const ctx = canvas.getContext("2d");
-let xoff1 = 0;
-let xoff2 = 10000;
+ctx.strokeStyle = 'blue';
 
-let time = Date.now();
+const inc = 0.01;
+const faster = 0.3;
+let start = 0;
 
-//https://www.youtube.com/watch?v=y7sgcFhk6ZM&list=PLRqwX-V7Uu6ZV4yEcW3uDwOgGXKUUsPOM&index=5
-// 6:00
-
-function renderLoop(now) {
-
-    let timeDelta = (now - time) / 1000;
+function renderLoop() {
 
     ctx.clearRect(0, 0, width, height);
-
     ctx.beginPath();
-    ctx.strokeStyle = 'blue';
-    ctx.moveTo(0, 0);
 
-    const x = noise(xoff1) * width;
-    const y = noise(xoff2) * height;
+    let xoff = start;
 
     for (let x = 0; x < width; x++){
+        let y = noise(xoff) * height;
         ctx.lineTo(x, y);
-        ellipse(x, y, 1);
+        ctx.stroke();
+        xoff += inc;
     }
 
-    // xoff += 20 * timeDelta;
-    xoff1 += 0.01;
-    xoff2 += 0.01;
-
-    ctx.closePath();
-    ctx.stroke();
-
-    time = now;
+    start += faster;
 
     requestAnimationFrame(renderLoop);
-}
-
-function distance (x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 }
 
 renderLoop();
